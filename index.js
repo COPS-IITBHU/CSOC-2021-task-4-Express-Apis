713f5cbbb4b1c056a4d11841fea8ed173419be36
K-Kumar-01 <kushalkumargupta4@gmail.com>
2021-07-11 22:54:52 +0530

    feat: setup mongodb connection
    Signed-off-by: K-Kumar-01 <kushalkumargupta4@gmail.com>

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require("express");
-const { json } = require("body-parser");
+const { json, urlencoded } = require("body-parser");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(json());
+app.use(urlencoded({ extended: true }));
 app.use(cors());
 
+// disable powered by cookies
+app.disable("x-powered-by");
+
 const PORT = process.env.PORT || 8000;
-const server = app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const mongoDB = "mongodb://127.0.0.1/my_database";
+
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose
+  .connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err.message));
