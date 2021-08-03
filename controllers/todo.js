156bad88d3ac8a00a81fc14425857780a52ba501
Dev Raj R <dev.raj.r1729@gmail.com>
2021-08-03 20:06:26 +0530

    implemented  ToDo functions

--- controllers/todo.js
+++ controllers/todo.js
@@ -36,10 +36,26 @@ const createToDo = async (req, res) => {
 
 const getParticularToDo = async (req, res) => {
   // Get the Todo of the logged in user with given id.
+  console.log(req.user);
+  ToDo.findById(req.params.id,'_id title',(err,todo)=>{
+    res.status(200).json(
+      {
+        id: todo.id,
+        title: todo.title
+      }
+    )
+  })
 };
 
 const editToDo = async (req, res) => {
   // Change the title of the Todo with given id, and get the new title as response.
+  ToDo.findById(req.params.id,(err,todo)=>{
+    todo.title = req.body.title;
+    todo.save().then(todo=>res.status(200).json({
+      id: todo.id,
+      title: todo.title
+    }));
+  });
 };
 
 const editToDoPatch = async (req, res) => {
@@ -48,6 +64,12 @@ const editToDoPatch = async (req, res) => {
 
 const deleteToDo = async (req, res) => {
   //  Delete the todo with given id
+  ToDo.findByIdAndDelete(req.params.id,(err,_)=>{
+    if (_ != null){
+      return res.sendStatus(204);
+    }
+    res.sendStatus(404);
+  })
 };
 
 module.exports = {
