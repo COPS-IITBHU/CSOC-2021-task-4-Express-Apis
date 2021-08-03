e6378879e1d03bb5e930b4f58f06def1963b5173
Dev Raj R <dev.raj.r1729@gmail.com>
2021-08-03 19:08:40 +0530

    added authenticate middleware

---
+++ utils/authenticate.js
@@ -0,0 +1,25 @@
+const { Token, User } = require("../models");
+
+const authenticateToken = (req,res,next) => {
+    const authHeader = req.headers.authorization;
+
+    if(authHeader && authHeader.split(' ')[0] == 'token') {
+        const token = authHeader.split(' ')[1];
+        Token.findOne({token},'user',(err,tokenObject)=>{
+            User.findById(tokenObject.user,'_id name email username',(err,user)=>{
+                req.user = {
+                    id: user.id,
+                    name:user.name,
+                    email: user.email,
+                    username: user.username
+                };
+                return next();
+            })
+        })
+    } else {
+        res.sendStatus(401);
+    }
+    res.sendStatus(401);
+}
+
+module.exports = authenticateToken
