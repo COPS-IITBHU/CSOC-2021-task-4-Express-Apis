275feba28e0438b97762e16e5568eaae5cf249f3
Dev Raj R <dev.raj.r1729@gmail.com>
2021-08-03 18:39:18 +0530

    added ToDoController routes

--- routes/todo.js
+++ routes/todo.js
@@ -3,6 +3,9 @@ const { ToDoController } = require("../controllers");
 const router = Router();
 
 router.get("/", ToDoController.getAllToDo);
-router.post("/:id/", ToDoController.createToDo);
-
+router.get("/:id/", ToDoController.getParticularToDo);
+router.post("/create/",ToDoController.createToDo);
+router.patch("/:id/",ToDoController.editToDoPatch);
+router.put("/:id/",ToDoController.editToDo);
+router.delete("/:id/",ToDoController.deleteToDo);
 // TODO: Create the end points similarly
