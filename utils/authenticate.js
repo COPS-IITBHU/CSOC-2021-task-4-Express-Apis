4b1d8b18f096ab408ff8723cf3e99f1d36704074
Dev Raj R <dev.raj.r1729@gmail.com>
2021-08-07 14:58:56 +0530

    error handling

--- utils/authenticate.js
+++ utils/authenticate.js
@@ -3,10 +3,22 @@ const { Token, User } = require("../models");
 const authenticateToken = (req,res,next) => {
     const authHeader = req.headers.authorization;
 
-    if(authHeader && authHeader.split(' ')[0] == 'token') {
+    if(authHeader && 
+        authHeader.split(' ')[0] == 'token' && 
+        authHeader.split(' ').length == 2) {
         const token = authHeader.split(' ')[1];
         Token.findOne({token},'user',(err,tokenObject)=>{
+            if (token==null) {
+                return res.status(401).json({
+                    error: "Invalid Token"
+                });
+            }
             User.findById(tokenObject.user,'_id name email username',(err,user)=>{
+                if (user==null) {
+                    return res.status(401).json({
+                        error: "User with that token no longer exists"
+                    });
+                }
                 req.user = {
                     id: user.id,
                     name:user.name,
@@ -14,8 +26,8 @@ const authenticateToken = (req,res,next) => {
                     username: user.username
                 };
                 return next();
-            })
-        })
+            });
+        });
     } else {
         res.sendStatus(401);
     }
