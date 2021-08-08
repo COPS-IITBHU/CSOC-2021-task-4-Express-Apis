1db4e753c1c2754371fe9e57d090138dcbaf6f9d
Swayamshu Swain <swayamshu2k@gmail.com>
2021-08-08 12:14:15 +0530

    final code

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const { User, Token } = require("../models");
 const { randomBytes } = require("crypto");
+const bcrypt = require("bcrypt");
 
 const createToken = (user) => {
   return Token({
@@ -9,24 +10,53 @@ const createToken = (user) => {
 };
 
 const login = async (req, res) => {
-  // TODO: Read username, pwd from the req object
-  // Check if data is valid
-  // Return correct status codes: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
-  // If the user is verified, then return a token along with correct status code
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    res.status(400).send('User fields are empty!');
+  } else {
+    const user = await User.findOne({ username: username });
+    if (user) {
+      const validPass = await bcrypt.compare(password, user.password);
+
+      if (validPass) {
+        const token = await Token.findOne({ user: user._id });
+        res.status(200).json({ token: token.token });
+      } else {
+        res.status(401).send('Wrong password!'); 
+      }
+    } else {
+      res.status(404).send('User not found!');
+    }
+  }
 };
 
 const signup = async (req, res) => {
-  // TODO: Read username, email, name, pwd from the req object
-  // Hash the password
-  // Return with appropriate status code in case of an error
-  // If successful, return with an appropriate token along with correct status code
+  const { username, email, name, password } = req.body;
+
+  if (!username || !email || !name || !password) {
+    res.status(400).send('User fields cannot be empty!');
+  } else {
+    const hash = await bcrypt.hash(password, 10);
+    
+    const user = new User({
+      name: name,
+      email: email,
+      username: username,
+      password: hash
+    })
+    await user.save();
+
+    const token = createToken(user);
+    await token.save();
+
+    res.status(200).json({ token: token.token });
+  }
 };
 
 const profile = async (req, res) => {
-  // TODO:
-  // Implement the functionality to retrieve the details
-  // of the logged in user.
-  // Check for the token and then use it to get user details
+  const { id, name, email, username } = req.user;
+  res.status(200).json({ id: id, name: name, email: email, username: username }); 
 };
 
 module.exports = { login, signup, profile };
