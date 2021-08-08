7121aeb50563c20da0d051fd89a2abfea0a5783d
Dev Raj R <dev.raj.r1729@gmail.com>
2021-08-08 19:18:28 +0530

    body validation

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,18 @@
 const { Router } = require("express");
 const { UserController } = require("../controllers");
-
+const {validate} = require("../utils");
 const router = Router();
 
-router.post("/login/", UserController.login);
-router.post("/signup", UserController.signup);
+router.post("/login/",validate.validateBody({
+    username: "string",
+    password: "string"
+}), UserController.login);
+router.post("/signup",validate.validateBody({
+    email: "string",
+    password: "string",
+    name: "string",
+    username: "string"
+}), UserController.signup);
 router.get("/profile", UserController.profile);
 
 module.exports = router;
